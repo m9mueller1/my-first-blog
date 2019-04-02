9cb25d7e9559ce2c9df47e5e492a8f9953e54e49
Scott <smueller@austin.utexas.edu>
2019-04-02 09:09:21 -0500

    Updated views.py

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,5 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+#    post = Post.objects.filter(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
